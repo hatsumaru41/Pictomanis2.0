c001c1c4fb883e047bf0a46307464eb01009d106
hatsumaru41 <81270310+hatsumaru41@users.noreply.github.com>
2022-09-23 21:20:29 -0500

    Crea edita

---
+++ src/app/page/categorias/categorias-creaedita/categorias-creaedita.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-categorias-creaedita',
+  templateUrl: './categorias-creaedita.component.html',
+  styleUrls: ['./categorias-creaedita.component.css']
+})
+export class CategoriasCreaeditaComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ src/app/page/usuario/usuario-creaedita/usuario-creaedita.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-usuario-creaedita',
+  templateUrl: './usuario-creaedita.component.html',
+  styleUrls: ['./usuario-creaedita.component.css']
+})
+export class UsuarioCreaeditaComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
