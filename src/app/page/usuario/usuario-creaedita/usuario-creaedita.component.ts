93a72268087c35f133b0f225de2db5a4681148f0
RenaPad <89104868+RenaPad@users.noreply.github.com>
2022-09-30 00:16:03 -0500

    Ya edita denuevo

--- src/app/page/usuario/usuario-creaedita/usuario-creaedita.component.ts
+++ src/app/page/usuario/usuario-creaedita/usuario-creaedita.component.ts
@@ -1,8 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
+import { ActivatedRoute, Params, Router } from '@angular/router';
 import { usuario } from 'src/app/model/usuario';
 import { UsuarioService } from 'src/app/service/usuario.service';
-
 @Component({
   selector: 'app-usuario-creaedita',
   templateUrl: './usuario-creaedita.component.html',
@@ -11,23 +10,49 @@ import { UsuarioService } from 'src/app/service/usuario.service';
 export class UsuarioCreaeditaComponent implements OnInit {
   Usuario: usuario = new usuario();
   mensaje: string = " ";
+  edicion: boolean = false;
+  id: number = 0;
 
-  constructor(private usuarioService: UsuarioService, private router: Router) { }
+  constructor(private UsuarioService: UsuarioService, private router: Router, private route: ActivatedRoute) { }
 
   ngOnInit(): void {
+    this.route.params.subscribe((data: Params) => {
+      this.id = data['id'];
+      this.edicion = data['id'] != null;
+      this.init();
+    });
   }
-  aceptar(): void {
-    if (this.Usuario.name.length > 0) {
 
-      this.usuarioService.insertar(this.Usuario).subscribe(data => {
-        this.usuarioService.listar().subscribe(data => {
-          this.usuarioService.setLista(data);
+
+  aceptar(): void {
+    if (this.Usuario.name.length > 0 && this.Usuario.email.length > 0) {
+      if (this.edicion) {
+        this.UsuarioService.modificar(this.Usuario).subscribe(data => {
+          this.UsuarioService.listar().subscribe(data => {
+            this.UsuarioService.setLista(data);
+          })
         })
-      })
-      this.router.navigate(['usuario']);
+      } else {
+
+        this.UsuarioService.insertar(this.Usuario).subscribe(data => {
+          this.UsuarioService.listar().subscribe(data => {
+            this.UsuarioService.setLista(data);
+          })
+        })
+      }
+      this.router.navigate(['propietarios']);
     } else {
       this.mensaje = "Complete los valores requeridos";
     }
   }
 
+
+  init() {
+    if (this.edicion) {
+      this.UsuarioService.listarId(this.id).subscribe(data => {
+        this.Usuario = data;
+      })
+    }
+
+  }
 }
