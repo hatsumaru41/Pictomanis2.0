db9a4d2b034bb3b5c887212c57ffd9517b2c46da
lucasm20 <93146688+lucasm20@users.noreply.github.com>
2022-11-25 00:53:13 -0500

    usuario

--- src/app/model/usuario.ts
+++ src/app/model/usuario.ts
@@ -1,3 +1,5 @@
+
+
 export class usuario {
   idUsuario: number = 0;
   nameUsuario: string = "";
@@ -5,4 +7,5 @@ export class usuario {
   numberUsuario:number = 0;
   emailUsuario:string= "";
   passwordUsuario:string = "";
+  
 }
